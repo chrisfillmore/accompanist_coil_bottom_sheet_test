3a1e8d16cd12ba8ccbca7fa158634272ad33ea26
Chris Fillmore <chrisf@stageten.tv>
2021-11-16 15:34:51 -0500

    Reduce to show a fixed height fixes the issue

--- app/src/main/java/ca/chrisfillmore/accompanistbottomsheettest/MainActivity.kt
+++ app/src/main/java/ca/chrisfillmore/accompanistbottomsheettest/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
@@ -77,55 +78,26 @@ class MainActivity : ComponentActivity() {
                   .weight(1f)
                   .fillMaxWidth(),
                 onClick = {
-                  navController.navigate(Screen.BottomSheetImageAndText.name)
+                  navController.navigate(Screen.BottomSheetImageWithHeight.name)
                 },
               ) {
                 Text(
                   fontSize = 4.em,
-                  text = "Open bottom sheet Image with fixed size",
+                  text = "Open bottom sheet Image with height",
                 )
               }
             }
           }
 
           bottomSheet(route = Screen.BottomSheetImage.name) {
-            // I could not reproduce just by using an ImageBitmap
-//            Image(
-//              bitmap = ImageBitmap.imageResource(id = R.drawable.good_news),
-//              contentDescription = null,
-//            )
-
-            // I can reproduce by using Coil's rememberImagePainter and loading a Drawable,
-            // but it can take several (10-20) attempts.
-//            Image(
-//              painter = rememberImagePainter(
-//                data = ResourcesCompat.getDrawable(resources, R.drawable.good_news, null),
-//              ),
-//              contentDescription = null,
-//            )
-
-            // I can easily reproduce by loading a url. This reproduces the bug more frequently
-            // than not.
-            Image(
-              painter = rememberImagePainter(
-                data = "https://cdn.shopify.com/s/files/1/0463/6753/9356/products/IMG_20190711_134921.jpg?v=1631815297",
-              ),
-              contentDescription = null,
-            )
+            BabyImage()
           }
 
           bottomSheet(
-            route = Screen.BottomSheetImageAndText.name,
+            route = Screen.BottomSheetImageWithHeight.name,
           ) {
-            Column {
-              Image(
-                painter = rememberImagePainter(
-                  data = "https://cdn.shopify.com/s/files/1/0463/6753/9356/products/IMG_20190711_134921.jpg?v=1631815297",
-                ),
-                contentDescription = null,
-                modifier = Modifier.height(400.dp)
-              )
-            }
+            // Applying a fixed height fixes the issue
+            BabyImage(modifier = Modifier.height(400.dp))
           }
         }
       }
@@ -133,8 +105,19 @@ class MainActivity : ComponentActivity() {
   }
 }
 
+@Composable
+fun BabyImage(modifier: Modifier = Modifier) {
+  Image(
+    painter = rememberImagePainter(
+      data = "https://cdn.shopify.com/s/files/1/0463/6753/9356/products/IMG_20190711_134921.jpg?v=1631815297",
+    ),
+    contentDescription = null,
+    modifier = modifier
+  )
+}
+
 enum class Screen {
   StartDestination,
   BottomSheetImage,
-  BottomSheetImageAndText,
+  BottomSheetImageWithHeight,
 }
